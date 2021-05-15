6d634b544154720c98e17e1633d495ac2d326a67
Mateusz Marczuk <mateusz.marczuk@cybercom.com>
2021-05-15 20:58:43 +0200

    Add zip generation

--- app/main.py
+++ app/main.py
@@ -7,13 +7,33 @@ import json
 import requests
 import uuid
 import weakref
+import threading
+import time 
+import os
+import zipfile
+import enum
 
+class StateEnum(enum.Enum):
+    IN_PROGRESS = "in-progress"
+    COMPLETED = "completed"
+    ERROR = "error occured"
+    
 class ZipArchiver:
-    instances = set()
-
-    def __init__(self, name):
+    def __init__(self, name, urlList):
         self.name = name
-        self.state = "Not started"
+        self.state = StateEnum.IN_PROGRESS
+        self.urlList = urlList
+        self.thread = threading.Thread(target=self.createArchive, name=self.name)
+
+    def start_processing(self):
+        self.thread.start()
+
+    def createArchive(self):
+        for url in self.urlList:
+            r = requests.get(str(url), stream=True)
+            z = zipfile.ZipFile("/zip_archive/" + self.name + ".zip", "a", zipfile.ZIP_DEFLATED)
+            z.writestr(os.path.basename(url), r.content)
+        self.state = StateEnum.COMPLETED
 
 class UrlList(BaseModel):
     urls: List[str] = Field(..., min_items=1)
@@ -54,17 +74,16 @@ zipArchiver = {}
 @app.post("/api/archive/create")
 async def create_archive(urlList: UrlList):
     archiveHash = generate_hash()
-    zipArchiver[archiveHash] = ZipArchiver(archiveHash)
+    zipArchiver[archiveHash] = ZipArchiver(archiveHash, urlList.urls)
+    zipArchiver[archiveHash].start_processing()
     return {"archive_hash" : archiveHash}
 
 @app.get("/api/archive/status/{archive_hash}")
 async def get_status(archive_hash : str):
-    print(zipArchiver)
     try:
-        state = zipArchiver[archive_hash].state
+        state = zipArchiver[archive_hash].state.value
     except KeyError:
         raise HTTPException(status_code=404, detail="Requested hash no foud")
-
     return {"status" : state}
 
 @app.get("/")
