41aa3ea88523dcbf6d63127abbf5f93d603d55a2
Mateusz Marczuk <marczuk@live.it>
2021-05-17 14:44:23 +0200

    New approach - store in-progress objects in files

--- app/main.py
+++ app/main.py
@@ -7,28 +7,43 @@ import uuid
 import os
 
 from models import UrlList
-from zipArchiver import StateEnum, ZipArchiver
+from zipArchiver import ZipArchiver
 
 app = FastAPI()
-zipArchiver = {}
+
+@app.on_event("startup")
+async def check_for_unfinished():
+    fileList = os.listdir("/zip_archive/in-progress")
+    if(fileList) :
+        for file in fileList:
+            if os.path.isfile("/zip_archive/" + file + ".zip") :
+                os.remove("/zip_archive/" + file + ".zip")
+            with open("/zip_archive/in-progress/" + file) as f:
+                urls = f.read().splitlines()
+            zipArchiver = ZipArchiver(file, urls)
+            zipArchiver.start_processing()
 
 @app.post("/api/archive/create")
 async def create_archive(urlList: UrlList):
     archiveHash = str(uuid.uuid4())
-    zipArchiver[archiveHash] = ZipArchiver(archiveHash, urlList.urls)
-    zipArchiver[archiveHash].start_processing()
+    zipArchiver = ZipArchiver(archiveHash, urlList.urls)
+
+    urlFile = open("/zip_archive/in-progress/" + archiveHash, "w")
+    for url in urlList.urls:
+        urlFile.write(url + "\n")
+    urlFile.close()
+
+    zipArchiver.start_processing()
     return {"archive_hash" : archiveHash}
 
 @app.get("/api/archive/status/{archive_hash}")
 async def get_status(archive_hash : str):
-    try:
-        state = zipArchiver[archive_hash].state
-    except KeyError:
+    if os.path.isfile("/zip_archive/in-progress/" + archive_hash):
+        return {"status" : "in-progress"}
+    elif (os.path.isfile("/zip_archive/" + archive_hash + ".zip") and (os.path.isfile("/zip/archive/in-progress/" + archive_hash) is False)) :
+        return {"status" : "completed", "url" : "http://localhost/archive/get/" + archive_hash + ".zip"}
+    else:
         raise HTTPException(status_code = 404, detail = "Requested archive hash no foud")
-    if state in (StateEnum.IN_PROGRESS, StateEnum.ERROR):
-        return {"status" : state.value}
-    elif state is StateEnum.COMPLETED:
-        return {"status" : state.value, "url" : "http://localhost/archive/get/" + archive_hash + ".zip"}
 
 @app.get("/archive/get/{file_name}")
 async def get_archive(file_name : str):
--- app/zipArchiver.py
+++ app/zipArchiver.py
@@ -3,18 +3,15 @@ import threading
 import os
 import zipfile
 import enum
-
-class StateEnum(enum.Enum):
-    IN_PROGRESS = "in-progress"
-    COMPLETED = "completed"
-    ERROR = "error occured"
     
 class ZipArchiver:
     def __init__(self, name, urlList):
         self.name = name
-        self.state = StateEnum.IN_PROGRESS
         self.urlList = urlList
         self.thread = threading.Thread(target=self.create_archive, name=self.name)
+
+    def __del__(self):
+        pass
 
     def start_processing(self):
         self.thread.start()
@@ -22,7 +19,7 @@ class ZipArchiver:
     def send_webhook(self):
         webhookUrl = os.getenv('WEBHOOK_URL')
         if webhookUrl :
-            link = "localhost/archive/get/" + self.name + ".zip"
+            link = "http://localhost/archive/get/" + self.name + ".zip"
             r = requests.post(webhookUrl, json = {"link": link })
 
     def create_archive(self):
@@ -31,5 +28,5 @@ class ZipArchiver:
             r = requests.get(str(url), stream=True)
             z = zipfile.ZipFile(self.archivePath, "a", zipfile.ZIP_DEFLATED)
             z.writestr(os.path.basename(url), r.content)
-        self.state = StateEnum.COMPLETED
+        os.remove("/zip_archive/in-progress/" + self.name)
         self.send_webhook()
